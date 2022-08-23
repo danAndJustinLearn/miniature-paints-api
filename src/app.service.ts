d51b25aee34a530e94f3d7cad7d8a571625608f9
Justin Somers <c-justin.somers@charter.com>
2022-08-23 11:25:41 -0600

    initial commit

---
+++ src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getTest(): string {
+    // add database calls here
+    return 'I am test';
+  }
+
+}
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(3000);
+  console.log('listening on localhost:3000')
+}
+bootstrap();
