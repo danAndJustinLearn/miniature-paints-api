3ff25ceb3f65fcb186ac65e98cd3576ec48c6510
Daniel Halverson <c-dan.halverson@charter.com>
2022-10-16 18:41:04 -0600

    Adds mongoose import to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PaintServiceService } from './paint-service/paint-service.service';
 import { WhatIsThisModule } from './what-is-this/what-is-this.module';
 
 @Module({
-  imports: [WhatIsThisModule],
+  imports: [
+    WhatIsThisModule,
+    MongooseModule.forRoot('mongodb://localhost/nest')
+  ],
   controllers: [AppController],
   providers: [AppService, PaintServiceService],
 })
