3cca90da6c87e4eb1ad41a9d3b60ad8b57652e4d
Daniel Halverson <c-dan.halverson@charter.com>
2022-10-17 20:47:59 -0600

    Adds model entity

---
+++ src/models/model.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class Model {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    brandName: string;
+
+    @Column()
+    colorName: string;
+
+    @Column()
+    artistColorName: string;
+
+    @Column()
+    paintId: number;
+
+    @Column()
+    price: number;
+}
