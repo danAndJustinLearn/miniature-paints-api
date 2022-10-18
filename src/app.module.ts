27573032ca06df54cbb927a7d90eed80a2193464
Daniel Halverson <c-dan.halverson@charter.com>
2022-10-17 20:44:37 -0600

    Adds typeorm to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ModelsController } from './models/models.controller'
 import { ModelsService } from './models/models.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, ModelsController],
   providers: [AppService, ModelsService],
 })
